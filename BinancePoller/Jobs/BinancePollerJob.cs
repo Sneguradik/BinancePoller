a56b6b535dd81da4a13e20947ab126b79e69532a
Sneguradik <rrtr845@gmail.com>
2025-03-11 14:15:03 +0300

    Improved filtering

--- BinancePoller/Jobs/BinancePollerJob.cs
+++ BinancePoller/Jobs/BinancePollerJob.cs
@@ -27,7 +27,7 @@ public class BinancePollerJob(IBinanceService binanceService, IIndexService inde
             binanceCandles
                 .LastOrDefault()?.OpenTime
                 .AddMilliseconds(1)??DateTime.UtcNow;
-        
+        if (binanceCandles.Count == 0) return;
         var result = await indexService.SendCandlesAsync(binanceCandles, fetchItem.Url, cancellationToken);
         if (result) logger.LogInformation($"{fetchItem.Symbol}: Successfully sent");
         else logger.LogError($"{fetchItem.Symbol}: Failed to send");
